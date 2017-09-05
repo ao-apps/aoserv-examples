ae74a691e3f375a302a1c7a3227e07a677b35291
Dan Armstrong <dan@aoindustries.com>
2017-09-05 19:59:23 +0000

    May now control references_priv through APIs.

--- src/com/aoindustries/aoserv/examples/mysql/AddMySQLUser.java
+++ src/com/aoindustries/aoserv/examples/mysql/AddMySQLUser.java
@@ -59,7 +59,7 @@ final public class AddMySQLUser {
 		aoClient.addMySQLServerUser(username, mysqlServer, server, MySQLServerUser.ANY_LOCAL_HOST);
 
 		// Grant access to the database
-		aoClient.addMySQLDBUser(database, mysqlServer, server, username, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
+		aoClient.addMySQLDBUser(database, mysqlServer, server, username, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
 
 		// Commit the changes before setting the password
 		aoClient.waitForMySQLUserRebuild(server);
@@ -115,7 +115,7 @@ final public class AddMySQLUser {
 		MySQLDatabase md=ms.getMySQLDatabase(database);
 
 		// Grant access to the database
-		md.addMySQLServerUser(msu, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
+		md.addMySQLServerUser(msu, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
 
 		// Commit the changes before setting the password
 		ao.waitForMySQLUserRebuild();
