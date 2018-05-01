1ed95657cc9eb21b51cffee32e6e77af9f4ce494
Dan Armstrong <dan@aoindustries.com>
2018-05-01 14:36:56 +0000

    Removed Thread.interrupt() after catching InterruptedException inside of loops where thread interrupted status is not used or where throwing additional exception.

--- src/com/aoindustries/aoserv/examples/vnc/VncConsoleTunnel.java
+++ src/com/aoindustries/aoserv/examples/vnc/VncConsoleTunnel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2013, 2017 by AO Industries, Inc.,
+ * Copyright 2009-2013, 2017, 2018 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -180,8 +180,6 @@ public class VncConsoleTunnel implements Runnable {
 					Thread.sleep(10000);
 				} catch(InterruptedException err) {
 					logger.log(Level.WARNING, null, err);
-					// Restore the interrupted status
-					Thread.currentThread().interrupt();
 				}
 			}
 		}
